9e6ecdba59101fb799d578088f849757b423edb1
yurunull <kamomeyuru@gmail.com>
2025-02-15 11:23:52 +0900

    home作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Header from "./compoments/header";
 
 function App() {
   return (
+    <>
+    <Header />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/add.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RegisterPage() {
+  return (
+    <>
+    <div>
+      <h1>Register Your Mystery Location</h1>
+      {/* 登録フォームなど */}
+    </div>
+    </>
+  );
+}
+
+export default RegisterPage;
